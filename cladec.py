269f9ef9b317f38e1b609459ea8d55abecd6e239
Harshvardhan <harshv834@gmail.com>
2021-07-20 13:03:06 +0000

    commited experiment code, cleaning remaining

--- cladec.py
+++ cladec.py
@@ -1,18 +1,26 @@
 import numpy as np
 import torch.cuda.amp as tca
 import torch
-from torch import nn
 from models import decay
+from torch import nn
+import pdb
+from tqdm import tqdm
+from dutils import getnorm
+import torchvision.transforms as transforms
+
 
 class ClaDecNet(nn.Module):
-    def __init__(self, cfg,inShape,nFea):
+    def __init__(self, cfg, inShape, nFea):
         super(ClaDecNet, self).__init__()
         self.channel_mult = int(64)
-        self.expLinLay = len(inShape) == 2 #linear layer...
+        self.expLinLay = len(inShape) == 2  # linear layer...
 
-        dim = 32 if self.expLinLay else 32//int(inShape[-1]) #dimension of input
-        self.input_dim=np.prod(inShape[1:])
-        self.inFea=inShape[-2] if not self.expLinLay else 1
+        if cfg["ds"][0] == "TinyImgNet":
+            dim = 64 if self.expLinLay else 64 // int(inShape[-1])  # dimension of input
+        else:
+            dim = 32 if self.expLinLay else 32 // int(inShape[-1])  # dimension of input
+        self.input_dim = np.prod(inShape[1:])
+        self.inFea = inShape[-2] if not self.expLinLay else 1
         nLay = int(np.round(np.log2(dim) - 2))
 
         # Use batchnorm or bias? LeakyRelu or Relu   -->  Does not make much of a difference
@@ -21,113 +29,212 @@ class ClaDecNet(nn.Module):
         bn, bias = lambda x: nn.Identity(), True
         rel = lambda x: nn.ReLU()
 
-        if self.inFea==1: #There is no spatial dimension (or it is one) -> use a dense layer as the first layer
-            self.useDense=True
-            self.fc_output_dim = max(self.input_dim, self.channel_mult)  # number of input features
-            self.fc = nn.Sequential(nn.Linear(self.input_dim, self.fc_output_dim), nn.ReLU(True))#, nn.BatchNorm1d(self.fc_output_dim)
-        else: # The spatial extend is larger one, use a conv layer, otherwise have too many parameters
-            self.fc_output_dim = inShape[1] # number of input features
-            self.fc = nn.Sequential(nn.Conv2d(inShape[1],inShape[1], 3, stride=1,padding=1, bias=bias), nn.ReLU(True))  # , nn.BatchNorm1d(self.fc_output_dim)
-            self.useDense=False
+        if (
+            self.inFea == 1
+        ):  # There is no spatial dimension (or it is one) -> use a dense layer as the first layer
+            self.useDense = True
+            self.fc_output_dim = max(
+                self.input_dim, self.channel_mult
+            )  # number of input features
+            self.fc = nn.Sequential(
+                nn.Linear(self.input_dim, self.fc_output_dim), nn.ReLU(True)
+            )  # , nn.BatchNorm1d(self.fc_output_dim)
+        else:  # The spatial extend is larger one, use a conv layer, otherwise have too many parameters
+            self.fc_output_dim = inShape[1]  # number of input features
+            self.fc = nn.Sequential(
+                nn.Conv2d(inShape[1], inShape[1], 3, stride=1, padding=1, bias=bias),
+                nn.ReLU(True),
+            )  # , nn.BatchNorm1d(self.fc_output_dim)
+            self.useDense = False
 
-        self.deconv = [nn.ConvTranspose2d(self.fc_output_dim,self.channel_mult * (2**nLay), 4, stride=2,padding=1, bias=bias), bn(self.channel_mult * (2**nLay)),rel(None) ]
-        for j in range(nLay,0,-1):
-            self.deconv+=[nn.ConvTranspose2d(self.channel_mult * (2**j),self.channel_mult * (2**(j-1)), 4, stride=2,padding=1, bias = bias), bn(self.channel_mult * (2**(j-1))), rel(None)]
-        self.deconv.append(nn.ConvTranspose2d(self.channel_mult * 1,nFea, 4, stride=2, padding=1, bias = True))
+        self.deconv = [
+            nn.ConvTranspose2d(
+                self.fc_output_dim,
+                self.channel_mult * (2 ** nLay),
+                4,
+                stride=2,
+                padding=1,
+                bias=bias,
+            ),
+            bn(self.channel_mult * (2 ** nLay)),
+            rel(None),
+        ]
+        for j in range(nLay, 0, -1):
+            self.deconv += [
+                nn.ConvTranspose2d(
+                    self.channel_mult * (2 ** j),
+                    self.channel_mult * (2 ** (j - 1)),
+                    4,
+                    stride=2,
+                    padding=1,
+                    bias=bias,
+                ),
+                bn(self.channel_mult * (2 ** (j - 1))),
+                rel(None),
+            ]
+        self.deconv.append(
+            nn.ConvTranspose2d(
+                self.channel_mult * 1, nFea, 4, stride=2, padding=1, bias=True
+            )
+        )
         self.deconv = nn.Sequential(*self.deconv)
-        self.sig=nn.Sigmoid()
+        self.sig = nn.Sigmoid()
 
     def forward(self, x):
         if self.useDense:
             x = x.view(-1, self.input_dim)
             x = self.fc(x)
-            x = x.view(-1, self.fc_output_dim,self.inFea,self.inFea)
+            x = x.view(-1, self.fc_output_dim, self.inFea, self.inFea)
         else:
-            x=self.fc(x)
-        x=self.deconv(x)
-        x=self.sig(x) #This sometimes gives better visualization, but you need to take care to standardize inputs as well
+            x = self.fc(x)
+        x = self.deconv(x)
+        x = self.sig(
+            x
+        )  # This sometimes gives better visualization, but you need to take care to standardize inputs as well
         return x
 
-def getClaDec(cfg,netCl,norm,train_dataset):
-    alpha=cfg["alpha"]
-    closs,cclloss,crloss, teaccs, trep, clloss, clr = 0,0,0, [], cfg["opt"][1], nn.CrossEntropyLoss(), cfg["opt"][2]
+
+def getClaDec(cfg, netCl, norm, train_dataset):
+    alpha = cfg["alpha"]
+    closs, cclloss, crloss, teaccs, trep, clloss, clr = (
+        0,
+        0,
+        0,
+        [],
+        cfg["opt"][1],
+        nn.CrossEntropyLoss(),
+        cfg["opt"][2],
+    )
     print("Train CLaDec")
     scaler = tca.GradScaler()
-    d=next(iter(train_dataset))
-    netDec=ClaDecNet(cfg, d[2].shape, cfg["imCh"]).cuda()
+    d = next(iter(train_dataset))
+    netDec = ClaDecNet(cfg, d[2].squeeze(0).shape, cfg["imCh"]).cuda()
     netDec.train()
+    netCl.train()
     optimizerCl = torch.optim.Adam(netDec.parameters(), lr=0.0003, weight_decay=1e-5)
-    aeloss=nn.MSELoss()
-    ulo=lambda closs,totloss,i: 0.97 * closs + 0.03 * totloss.item() if epoch > 20 else 0.8 * closs + 0.2 * totloss.item()
-    for epoch in range(trep):
+    aeloss = nn.MSELoss()
+    ulo = (
+        lambda closs, totloss, i: 0.97 * closs + 0.03 * totloss.item()
+        if epoch > 20
+        else 0.8 * closs + 0.2 * totloss.item()
+    )
+    for epoch in tqdm(range(trep)):
         for i, data in enumerate(train_dataset):
             with tca.autocast():
                 optimizerCl.zero_grad()
-                dsx, dsy,dsact = data[0].cuda(), data[1].cuda(), data[2].cuda()
+                if cfg["ds"][0] == "TinyImgNet":
+                    dsx, dsy, dsact = (
+                        data[0].squeeze(0).cuda(),
+                        data[1].squeeze(0).cuda(),
+                        data[2].squeeze(0).cuda(),
+                    )
+                else:
+                    dsx, dsy, dsact = data[0].cuda(), data[1].cuda(), data[2].cuda()
+                #dsx, dsy, dsact = data[0].cuda(), data[1].cuda(), data[2].cuda()
+
                 output = netDec(dsact.float())
-                recloss = aeloss(output,dsx)
-                claloss=clloss(netCl(output),dsy.long())
-                totloss=(1-alpha)*recloss+alpha*claloss
+                recloss = aeloss(output, dsx)
+                if cfg["ds"][0] == "TinyImgNet":
+                    norm = getnorm("TinyImgNet")
+                    transform = transforms.Compose(
+                        [
+                            transforms.Normalize(mean=norm[0], std=norm[1]),
+                            transforms.RandomResizedCrop(64),
+                            transforms.RandomHorizontalFlip(),
+                        ]
+                    )
+                    claloss = clloss(netCl(transform(output)), dsy.long())
+                else:
+                    claloss = clloss(netCl(output), dsy.long())
+                # claloss = clloss(netCl(output), dsy.long())
+                totloss = (1 - alpha) * recloss + alpha * claloss
                 scaler.scale(totloss).backward()
                 scaler.step(optimizerCl)
                 scaler.update()
-                closs,cclloss,crloss=ulo(closs,totloss,epoch),ulo(cclloss,claloss,epoch),ulo(crloss,recloss,epoch)
+                closs, cclloss, crloss = (
+                    ulo(closs, totloss, epoch),
+                    ulo(cclloss, claloss, epoch),
+                    ulo(crloss, recloss, epoch),
+                )
 
         decay(cfg["opt"], epoch, optimizerCl)
-        if (epoch % 2 == 0 and epoch <= 10) or (epoch % 10 == 0 and epoch > 10): print(epoch, np.round(np.array([closs,crloss,cclloss]), 5))
+        if (epoch % 2 == 0 and epoch <= 10) or (epoch % 10 == 0 and epoch > 10):
+            print(epoch, np.round(np.array([closs, crloss, cclloss]), 5))
 
     lcfg = {"ClaLo": closs}
     netDec.eval()
-    return netDec,lcfg
+    return netDec, lcfg
 
-def getActModel(cfg,classifier):
+
+def getActModel(cfg, classifier):
     ind = cfg["layInd"]
-    if ind<-1: ind=ind-2
+    if cfg["ds"][0] == "TinyImgNet":
+        if ind < -1:
+            ind = ind - 3
+    else:
+        if ind < -1:
+            ind = ind - 2
     actModel = nn.Sequential(*list(classifier.children())[:ind])
     actModel.eval()
     return actModel
 
+
 class RefAE(nn.Module):
-    def __init__(self, cfg,inShape):
+    def __init__(self, cfg, inShape):
         super(RefAE, self).__init__()
-        self.cladec=ClaDecNet(cfg, inShape, cfg["imCh"])
+        self.cladec = ClaDecNet(cfg, inShape, cfg["imCh"])
         self.cladec.train()
         from models import Classifier
-        cla=Classifier(cfg)
-        actModel=getActModel(cfg,cla)
-        actModel.train()
-        self.seq=nn.Sequential(actModel,self.cladec)
 
+        cla = Classifier(cfg)
+        actModel = getActModel(cfg, cla)
+        actModel.train()
+        self.seq = nn.Sequential(actModel, self.cladec)
 
     def forward(self, x):
         return self.seq(x)
 
 
-def getRefAE(cfg,train_dataset):
-    closs, teaccs, trep, clloss, clr = 0, [], cfg["opt"][1], nn.CrossEntropyLoss(), cfg["opt"][2]
+def getRefAE(cfg,acts ,train_dataset):
+    closs, teaccs, trep, clloss, clr = (
+        0,
+        [],
+        cfg["opt"][1],
+        nn.CrossEntropyLoss(),
+        cfg["opt"][2],
+    )
     print("Train RefAE")
     scaler = tca.GradScaler()
-    d=next(iter(train_dataset))
-    netDec=RefAE(cfg, d[2].shape).cuda()
+    d = next(iter(acts))
+    netDec = RefAE(cfg, d[2].squeeze(0).shape).cuda()
     netDec.train()
     optimizerCl = torch.optim.Adam(netDec.parameters(), lr=0.0003, weight_decay=1e-5)
-    aeloss=nn.MSELoss()
-    ulo=lambda closs,totloss,i: 0.97 * closs + 0.03 * totloss.item() if epoch > 20 else 0.8 * closs + 0.2 * totloss.item()
-    for epoch in range(trep):
+    aeloss = nn.MSELoss()
+    ulo = (
+        lambda closs, totloss, i: 0.97 * closs + 0.03 * totloss.item()
+        if epoch > 20
+        else 0.8 * closs + 0.2 * totloss.item()
+    )
+    for epoch in tqdm(range(trep)):
         for i, data in enumerate(train_dataset):
             with tca.autocast():
                 optimizerCl.zero_grad()
-                dsx, dsy = data[0].cuda(), data[1].cuda()
+                # if cfg["ds"][0] == "TinyImgNet":
+                #     dsx = data[0].squeeze(0).cuda()
+                # else:
+                #     dsx = data[0].cuda()
+                dsx = data[0].cuda()
                 output = netDec(dsx.float())
-                recloss = aeloss(output,dsx)
+                recloss = aeloss(output, dsx)
                 scaler.scale(recloss).backward()
                 scaler.step(optimizerCl)
                 scaler.update()
-                closs=ulo(closs,recloss,epoch)
+                closs = ulo(closs, recloss, epoch)
 
         decay(cfg["opt"], epoch, optimizerCl)
-        if (epoch % 2 == 0 and epoch <= 10) or (epoch % 10 == 0 and epoch > 10): print(epoch, np.round(np.array([closs]), 5))
+        if (epoch % 2 == 0 and epoch <= 10) or (epoch % 10 == 0 and epoch > 10):
+            print(epoch, np.round(np.array([closs]), 5))
 
     lcfg = {"RefAELo": closs}
     netDec.eval()
-    return netDec,lcfg
+    return netDec, lcfg
--- dutils.py
+++ dutils.py
@@ -1,48 +1,252 @@
-import torchvision,torch
+import torchvision, torch
 import torchvision.transforms as transforms
-from torch.utils.data import Dataset,TensorDataset
-import numpy as np,sklearn
+from torchvision.datasets import ImageFolder
+from torch.utils.data import Dataset, TensorDataset, DataLoader
+import numpy as np, sklearn
 import torch.nn.functional as F
+import os
+import requests
+import zipfile
+import io
+from shutil import copyfile
 
-gds = lambda dataset,cfg: torch.utils.data.DataLoader(TensorDataset(*[torch.from_numpy(x) for x in dataset]), batch_size=cfg["batchSize"])
+from torchvision.transforms.transforms import CenterCrop, Resize
+
+
+gds = lambda dataset, cfg: torch.utils.data.DataLoader(
+    TensorDataset(*[torch.from_numpy(x) for x in dataset]), batch_size=cfg["batchSize"]
+)
+
 
 def getnorm(dname):
-     if dname == "Ci10": return (torch.from_numpy(np.array((0.4914, 0.4822, 0.4465),np.float32).reshape(1,3,1,1)).cuda(), torch.from_numpy(np.array((0.2023, 0.1994, 0.2010),np.float32).reshape(1,3,1,1)).cuda())
-     elif dname == "Ci100": return (torch.from_numpy(np.array((0.5060725 , 0.48667726, 0.4421305),np.float32).reshape(1,3,1,1)).cuda() , torch.from_numpy(np.array((0.2675421,0.25593522,0.27593908),np.float32).reshape(1,3,1,1)).cuda())
-     elif dname == "Fash": return (torch.from_numpy(np.array((0.281),np.float32).reshape(1,1,1,1)).cuda() , torch.from_numpy(np.array((0.352),np.float32).reshape(1,1,1,1)).cuda())
+    if dname == "Ci10":
+        return (
+            torch.from_numpy(
+                np.array((0.4914, 0.4822, 0.4465), np.float32).reshape(1, 3, 1, 1)
+            ).cuda(),
+            torch.from_numpy(
+                np.array((0.2023, 0.1994, 0.2010), np.float32).reshape(1, 3, 1, 1)
+            ).cuda(),
+        )
+    elif dname == "Ci100":
+        return (
+            torch.from_numpy(
+                np.array((0.5060725, 0.48667726, 0.4421305), np.float32).reshape(
+                    1, 3, 1, 1
+                )
+            ).cuda(),
+            torch.from_numpy(
+                np.array((0.2675421, 0.25593522, 0.27593908), np.float32).reshape(
+                    1, 3, 1, 1
+                )
+            ).cuda(),
+        )
+    elif dname == "Fash":
+        return (
+            torch.from_numpy(np.array((0.281), np.float32).reshape(1, 1, 1, 1)).cuda(),
+            torch.from_numpy(np.array((0.352), np.float32).reshape(1, 1, 1, 1)).cuda(),
+        )
+    elif dname == "MNIST":
+        return (
+            torch.from_numpy(np.array((0.1307), np.float32).reshape(1, 1, 1, 1)).cuda(),
+            torch.from_numpy(np.array((0.3081), np.float32).reshape(1, 1, 1, 1)).cuda(),
+        )
+    elif dname == "TinyImgNet":
+        return (
+            torch.from_numpy(
+                np.array((0.4802, 0.4481, 0.3975), np.float32).reshape(3, 1, 1)
+            ),
+            torch.from_numpy(
+                np.array((0.2302, 0.2265, 0.2262), np.float32).reshape(3, 1, 1)
+            ),
+        )
 
-def getFullDS(cfg):
-    dname=cfg["ds"][0]
-    trans=transforms.Compose([transforms.ToTensor()])
-    if  dname== "Ci10":
-        cdat = torchvision.datasets.CIFAR10  # transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]) #transform = transforms.Compose([transforms.ToTensor(), norm])
+
+def getFullDS(cfg, reconstr=False):
+    dname = cfg["ds"][0]
+    trans = transforms.Compose([transforms.ToTensor()])
+    refu = lambda x: F.interpolate(x.unsqueeze(0), size=32).squeeze(0)
+    if dname == "Ci10":
+        cdat = (
+            torchvision.datasets.CIFAR10
+        )  # transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]) #transform = transforms.Compose([transforms.ToTensor(), norm])
         cfg["imCh"] = 3
     elif dname == "Ci100":
-        cdat = torchvision.datasets.CIFAR100  # transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        cdat = (
+            torchvision.datasets.CIFAR100
+        )  # transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
         cfg["imCh"] = 3
     elif dname == "Fash":
         cdat = torchvision.datasets.FashionMNIST
-        refu = lambda x: F.interpolate(x.unsqueeze(0), size=32).squeeze(0) #img = img - np.array([0.281])            img = img / np.array([0.352])
+        # img = img - np.array([0.281])            img = img / np.array([0.352])
         trans = transforms.Compose([transforms.ToTensor(), refu])
         cfg["imCh"] = 1
+    elif dname == "MNIST":
+        cdat = torchvision.datasets.MNIST
+        # trans = transforms.Compose([transforms.ToTensor(), refu, transforms.Normalize(0.1307,0.3081)])
+        trans = transforms.Compose([transforms.ToTensor(), refu])
+        cfg["imCh"] = 1
+    elif dname == "TinyImgNet":
+        cdat = tinyImgNet
+        norm = getnorm(dname)
+        # gamma_corr = lambda img : transforms.functional.adjust_gamma(img, gamma=, float=)
+        # resize_tr = random.choice([transforms.RandomResizedCrop(56), transforms.CenterCrop(56)])
+        # aug_tr = random.choice([transforms.RandomHorizontalFlip(), transforms.RandomAffine(30, translate=10, scale = [0.8])])
+        # data_augs = [transforms.RandomResizedCrop(32), transforms.RandomCrop(32), transforms.RandomHorizontalFlip(), transforms.RandomRotation(45)]
+        train_trans = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                #transforms.Normalize(mean=norm[0], std=norm[1]),
+                transforms.RandomResizedCrop(64),
+                # transforms.CenterCrop(56),
+                # transforms.Resize(32),
+                transforms.RandomHorizontalFlip(),
+            ]
+        )
+        val_trans = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                # transforms.CenterCrop(56),
+                #transforms.Normalize(mean=norm[0], std=norm[1]),
+                # transforms.Resize(32)
+            ]
+        )
+        cfg["imCh"] = 3
 
-    ntrain,down=cfg["ntrain"],True
-    def loadStore(isTrain,ndat):
-        nonlocal  cdat
-        trainset = cdat(root="data/", train=isTrain, download=down,transform=trans)
-        train_dataset = torch.utils.data.DataLoader(trainset, batch_size=ndat, num_workers=4)  # cfg["batchSize"]
+    ntrain, down = cfg["ntrain"], True
+    if dname == "TinyImgNet":
+        down = False
+        if reconstr == True:
+            trainset = cdat(
+                root="data/",
+                train=True,
+                download=down,
+                transform=transforms.Compose([transforms.ToTensor()]),
+            )
+            valset = cdat(
+                root="data/",
+                train=False,
+                download=down,
+                transform=transforms.Compose([transforms.ToTensor()]),
+            )
+
+        else:
+            trainset = cdat(
+                root="data/", train=True, download=down, transform=train_trans
+            )
+            valset = cdat(root="data/", train=False, download=down, transform=val_trans)
+        train_loader = DataLoader(
+            trainset, batch_size=cfg["batchSize"], shuffle=True, num_workers=4
+        )
+        val_loader = DataLoader(
+            valset, batch_size=cfg["batchSize"], shuffle=False, num_workers=4
+        )
+        return train_loader, val_loader, None, trainset, valset
+
+    def loadStore(isTrain, ndat):
+        nonlocal cdat
+        trainset = cdat(root="data/", train=isTrain, download=down, transform=trans)
+        train_dataset = torch.utils.data.DataLoader(
+            trainset, batch_size=ndat, num_workers=4
+        )  # cfg["batchSize"]
         ds = next(iter(train_dataset))
         X, Y = ds[0].clone().numpy(), ds[1].clone().numpy()
-        #normA = lambda bx: (bx - np.min(bx)) / (np.max(bx) - np.min(bx) + 1e-10)  # "Normalizing - necessary if no BN"
-        #X,Y = normA(X), normA(Y)
+        # normA = lambda bx: (bx - np.min(bx)) / (np.max(bx) - np.min(bx) + 1e-10)  # "Normalizing - necessary if no BN"
+        # X,Y = normA(X), normA(Y)
         ds = [X, Y]
-        print("Data stats", cdat, X.shape, np.mean(X, axis=(0, 2, 3)), np.std(X, axis=(0, 2, 3)),np.max(X),np.min(X), " Data should be normalized")
+        print(
+            "Data stats",
+            cdat,
+            X.shape,
+            np.mean(X, axis=(0, 2, 3)),
+            np.std(X, axis=(0, 2, 3)),
+            np.max(X),
+            np.min(X),
+            " Data should be normalized",
+        )
         ds = sklearn.utils.shuffle(*ds)
-        return  ds[0].astype(np.float16), ds[1].astype(np.int16)
+        return ds[0].astype(np.float16), ds[1].astype(np.int16)
 
-    trX,trY=loadStore(True,ntrain)
-    teX, teY=loadStore(False, ntrain//2)
-    def cds(trX,trY,shuffle=True):
-        ds=TensorDataset(torch.from_numpy(trX), torch.from_numpy(trY))
-        return torch.utils.data.DataLoader(ds, batch_size=cfg["batchSize"], shuffle=shuffle, num_workers=4)
-    return cds(trX, trY), cds(teX, teY,False),None
+    trX, trY = loadStore(True, ntrain)
+    teX, teY = loadStore(False, ntrain // 2)
+
+    def cds(trX, trY, shuffle=True):
+        ds = TensorDataset(torch.from_numpy(trX), torch.from_numpy(trY))
+        return DataLoader(
+            ds, batch_size=cfg["batchSize"], shuffle=shuffle, num_workers=4
+        )
+
+    return cds(trX, trY), cds(teX, teY, False), None
+
+
+def tinyImgNet(root: str, train: bool, download: bool, transform) -> Dataset:
+    filter_val = train and not download
+
+    if download:
+        os.makedirs(root, exist_ok=True)
+        r = requests.get(
+            "http://cs231n.stanford.edu/tiny-imagenet-200.zip", stream=True
+        )
+        z = zipfile.ZipFile(io.BytesIO(r.content))
+        z.extractall(root)
+
+    main_dir_path = os.path.join(root, "tiny-imagenet-200")
+    train_path = os.path.join(main_dir_path, "train")
+    val_path = os.path.join(main_dir_path, "val")
+    if not (os.path.exists(train_path) and os.path.exists(val_path)):
+        raise ValueError("Train and Val paths don't exist")
+
+    val_formatted = os.path.join(main_dir_path, "val_formatted")
+    if not filter_val and not os.path.exists(val_formatted):
+        with open(os.path.join(val_path, "val_annotations.txt"), "r") as f:
+            img_class_map = f.readlines()
+        for img_class in img_class_map:
+            img_name, class_name = img_class.split("\t")[0], img_class.split("\t")[1]
+            img_path = os.path.join("images", img_name)
+            class_dir = os.path.join(val_formatted, class_name)
+            os.makedirs(os.path.join(class_dir, "images"), exist_ok=True)
+            copyfile(
+                os.path.join(val_path, img_path), os.path.join(class_dir, img_path)
+            )
+    if train:
+        return ImageFolder(train_path, transform)
+    else:
+        return ImageFolder(val_formatted, transform)
+
+
+from torchvision.datasets.folder import DatasetFolder
+import torch
+from typing import Any, Callable, cast, Dict, List, Optional, Tuple
+import numpy as np
+
+class NumpyFolder(DatasetFolder):
+    def __init__(
+            self,
+            root: str,
+            transform: Optional[Callable] = None,
+            target_transform: Optional[Callable] = None,
+            loader: Callable[[str], Any] = np.load,
+            is_valid_file: Optional[Callable[[str], bool]] = None,):
+        super(NumpyFolder, self).__init__(root, loader, (".npy") if is_valid_file is None else None,
+                                          transform=transform,
+                                          target_transform=target_transform,
+                                          is_valid_file=is_valid_file)
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (sample, target) where target is class_index of the target class.
+        """
+        path, target = self.samples[index]
+        (act,img) = self.loader(path, allow_pickle=True)
+        img = torch.Tensor(img).type(torch.float16)
+        act = torch.Tensor(act).type(torch.float16)
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target, act
